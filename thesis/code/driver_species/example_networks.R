129965f7e4a5448b014f08fde07217da8b33fda7
Fernando Cagua <efcaguab@gmail.com>
2019-03-26 17:53:03 +1300

    Rough addition of figure 1 of driver species chapter

---
+++ thesis/code/driver_species/example_networks.R
@@ -0,0 +1,116 @@
+# This functions can be called to provide the networks that are used for the
+# example figures in the driver species chapter
+
+example_ntw_structural <- function(){
+  en_controllable <- list(
+    edges = dplyr::data_frame(from = c("x_1", "x_2", "u_1"),
+                              to   = c("x_2", "x_3", "x_1"),
+                              control_type = c("a", "a", "b")),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "u_1"),
+                               control_type = c("a", "a", "a", "b"))
+  )
+
+  en_inaccessible <- list(
+    edges = dplyr::data_frame(from = c("x_1", "x_3", "u_1"),
+                              to   = c("x_2", "x_2", "x_1"),
+                              control_type = c("a", "a", "b")),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "u_1"),
+                               control_type = c("a", "a", "a", "b"))
+  )
+
+  en_dilation <- list(
+    edges = dplyr::data_frame(from = c("x_2", "x_2", "u_1"),
+                              to   = c("x_1", "x_3", "x_2"),
+                              control_type = c("a", "a", "b")),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "u_1"),
+                               control_type = c("a", "a", "a", "b"))
+  )
+
+  en_inaccessible_ok <- add_vertex_edge(en_inaccessible, "u_2", "b", "u_2", "x_3", "b")
+  en_dilation_ok <- add_vertex_edge(en_dilation, "u_2", "b", "u_2", "x_3", "b")
+
+  list(en_controllable, en_inaccessible, en_inaccessible_ok, en_dilation, en_dilation_ok) %>%
+    purrr::map(~ igraph::graph_from_data_frame(.$edges, directed = T, vertices = .$vertex))
+
+}
+
+
+example_ntw_directions <- function(){
+  en_vis <- list(
+    edges = dplyr::data_frame(from = c("p_1", "p_2", "p_2", "p_3", "p_3"),
+                              to = c("a_1", "a_1", "a_2", "a_2", "a_1"),
+                              weight = c(1, 2, 3, 1, 1)),
+    vertex = dplyr::data_frame(name = c("p_1", "p_2", "p_3", "a_1", "a_2"),
+                               type = c("p", "p", "p", "a", "a"))
+  )
+
+  en_bip <- igraph::graph_from_data_frame(en_vis$edges, directed = F, vertices = en_vis$vertex)
+  en_dir <- as_directed_network(en_bip, direction = "asymmetry", ties = "both", higher_level = "a")
+  # plot(en_dir)
+  # length(all_matchings(en_dir))
+
+  list(en_bip, en_dir)
+}
+
+example_ntw_matchings_chain <- function(){
+  list(
+    edges = dplyr::data_frame(from = c("x_1", "x_2", "x_3"),
+                              to =   c("x_2", "x_3", "x_4"),
+                              control_type = c(rep("a", 3))),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "x_4"),
+                               control_type = c(rep("a", 4)))
+  ) %$%
+    igraph::graph_from_data_frame(edges, directed = T, vertices = vertex)
+}
+
+
+example_ntw_matchings_star <- function(){
+  list(
+    edges = dplyr::data_frame(from = c("x_1", "x_1", "x_1"),
+                              to   = c("x_2", "x_3", "x_4" )),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "x_4"))
+  ) %$%
+    igraph::graph_from_data_frame(edges, directed = T, vertices = vertex)
+}
+
+matchings_example_networks <- function(x){
+  x %>%
+    all_matchings() %>%
+    purrr::map(add_control_nodes)
+}
+
+example_ntw_bidirection_only <- function(){
+  list(
+    edges = dplyr::data_frame(from = c("x_1", "x_2"),
+                              to   = c("x_2", "x_1")),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2"))
+  ) %$%
+    igraph::graph_from_data_frame(edges, directed = T, vertices = vertex)
+}
+
+example_ntw_bidirection_embedded <- function(){
+  list(
+    edges = dplyr::data_frame(from = c("x_1", "x_2", "x_1", "x_3", "x_1"),
+                              to   = c("x_2", "x_1", "x_3", "x_1", "x_4")),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "x_4"))
+  ) %$%
+    igraph::graph_from_data_frame(edges, directed = T, vertices = vertex)
+}
+
+example_ntw_bidirection_double <- function(){
+  list(
+    edges = dplyr::data_frame(from = c("x_1", "x_2", "x_1", "x_3", "x_1", "x_5", "x_6"),
+                              to   = c("x_2", "x_1", "x_3", "x_1", "x_4", "x_6", "x_5")),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "x_4", "x_5", "x_6"))
+  ) %$%
+    igraph::graph_from_data_frame(edges, directed = T, vertices = vertex)
+}
+
+example_ntw_bidirection_triple <- function(){
+  list(
+    edges = dplyr::data_frame(from = c("x_1", "x_2", "x_1", "x_3", "x_1", "x_5", "x_6", "x_7"),
+                              to   = c("x_2", "x_1", "x_3", "x_1", "x_4", "x_6", "x_5", "x_8")),
+    vertex = dplyr::data_frame(name = c("x_1", "x_2", "x_3", "x_4", "x_5", "x_6", "x_7", "x_8"))
+  ) %$%
+    igraph::graph_from_data_frame(edges, directed = T, vertices = vertex)
+}
---
+++ thesis/code/driver_species/fig_1_struct_control.R
@@ -0,0 +1,452 @@
+make_fig_structural_control <- function(en_direction, en_structural, pdf_out = NULL){
+
+  # graph attributes
+  formatted_en_direction <- en_direction  %>%
+    purrr::map(ntw_format_theme)
+
+  formatted_en_direction[[1]] %<>%
+    igraph::set_edge_attr("width", value = igraph::E(.)$weight)
+  formatted_en_direction[[2]] %<>%
+    igraph::set_edge_attr("width", value = 1)
+
+  # format networks for plotting
+  formatted_en_structural <- en_structural %>%
+    purrr::map(ntw_format_theme) %>%
+    purrr::map(ntw_control_network_theme)
+
+  adjust_darker_bg <- . %>%
+    add_property("vertex", "color", "control_type", "type == 'b' ~ get_color('bg_dark')", "TRUE ~ get_color('bg')") %>%
+    add_property("vertex", "frame.color", "control_type", "type == 'b' ~ get_color('bg_dark')", "TRUE ~ 'black'")
+
+  formatted_en_structural[[3]] %<>% adjust_darker_bg()
+  formatted_en_structural[[5]] %<>% adjust_darker_bg()
+
+  box_col <- get_color("bg")
+  box_col_2 <- get_color("bg_dark")
+
+  heading_height <- 0.20
+  heading_thin_height <- 0.15
+  div_height <- 0.05
+  inner_margin_height <- 0.05
+  plot_1_height <- 0.75
+  plot_2_height <- 0.7
+  legend_height <- 0.25
+  heights <- c(heading_height, plot_1_height,
+               div_height,
+               heading_thin_height,
+               plot_2_height,
+               div_height, plot_2_height,
+               legend_height)
+  d <- 30
+  x <- 3.1
+  margin_h <- (fig_sizes()$one_column_width-x)
+  widths <- c(margin_h, x/3-x/2/d, x/d, x/2-x/3-x/d, x/d, x/2-x/3-x/d/2, x/3,0.001)
+
+  # pdf(pdf_out, width = sum(widths), height = sum(heights), paper='special')
+  # if(is.null(pdf_out)) dev.control("enable")
+
+  c(  01,10,10,10,12,11,11,91,
+      90,13,13,13,12,14,14,91,
+      80,80,80,80,80,80,80,80,
+      22,20,20,20,28,21,21,93,
+      22,24,24,24,28,25,25,94,
+      94,81,81,81,81,81,81,81,
+      23,26,26,26,28,27,27,93,
+      30,30,30,30,30,30,30,30) %>%
+    dplyr::dense_rank() %>%
+    matrix(ncol = 8, byrow = T) %>%
+    layout(heights = heights,
+           widths = widths)
+  par(mar = rep(0,4), bg = "white", xpd = NA)
+
+  plot.new()
+
+  rect(grconvertX(margin_h, "inches", "user"),
+       grconvertY(sum(heights) - plot_1_height - heading_height, "inches", "user"),
+       grconvertX(3.5, "inches", "user"),
+       grconvertY(sum(heights) , "inches", "user"),
+       col = box_col,
+       border = NA)
+
+  rect(grconvertX(margin_h, "inches", "user"),
+       grconvertY(legend_height, "inches", "user"),
+       grconvertX(3.5, "inches", "user"),
+       grconvertY(plot_2_height * 2 + div_height + heading_thin_height + legend_height, "inches", "user"),
+       col = box_col,
+       border = NA)
+
+  # rect(grconvertX(margin_h, "inches", "user"),
+  #      grconvertY(plot_2_height  + legend_height +  heading_thin_height, "inches", "user"),
+  #      grconvertX(3.5, "inches", "user"),
+  #      grconvertY(plot_2_height * 2 + div_height + heading_thin_height + legend_height, "inches", "user"),
+  #      col = box_col_2,
+  #      border = NA)
+
+  rect(grconvertX(margin_h, "inches", "user"),
+       grconvertY(legend_height, "inches", "user"),
+       grconvertX(3.5, "inches", "user"),
+       grconvertY(plot_2_height + div_height/2+ legend_height, "inches", "user"),
+       col = box_col_2,
+       border = NA)
+
+
+  # 02
+  # standalone_text("(b)", y = 0.75)
+  # plot.new()
+  ## FIGURE A
+  # 10
+  standalone_text("Visitation network", y = 0.25, adj = c(0.5,0), font = 2)
+  text(0, 1, "(a)", adj = c(0.15,1.5), font = 1)
+
+  # 11
+  standalone_text("Direction of control", y = 0.25, adj = c(0.5,0), font = 2)
+
+  # 12
+  # standalone_vline(lty = 2)
+  plot.new()
+  # 13-14
+  for(i in 1:length(formatted_en_direction)){
+    l <- rescale_layout(direction_layout(), xlim = c(-1, 1) * 1.6, ylim = c(-0.55, 0.7))
+    plot_example_ntw(formatted_en_direction[[i]], layout = l)
+  }
+  ## FIGURE B
+  # 20
+  standalone_text("Dilation", y = 0.5, adj = c(0.5,1), font = 1)
+  text(0, 1, "(b)", adj = c(0.15,1.5), font = 1)
+  # 21
+  standalone_text("Inaccessible node", y = 0.5, adj = c(0.5,1), font = 1)
+  # 22
+  standalone_text("Not\ncontrollable", srt = 90, font = 2)
+  # 23
+  standalone_text("Controllable", srt = 90, font = 2)
+  # 24-27
+  for (i in c(4,2,5,3)){
+    # if(i == 3)   text(-2.26, 1, "(b)", adj = c(0,1.3), font = 1)
+
+    types <- igraph::V(formatted_en_structural[[i]])$control_type == dplyr::first(igraph::V(formatted_en_structural[[i]])$control_type)
+    l <- igraph::layout_as_bipartite(formatted_en_structural[[i]], types) %>%
+      rescale_layout(xlim = c(-1, 1) * 1.55, ylim = c(-0.6, 0.65))
+    plot_example_ntw(formatted_en_structural[[i]], layout = l)
+  }
+  # 28
+  # standalone_vline(lty = 2)
+  plot.new()
+
+  ## DIV LINES
+  # 80
+  # standalone_hline()
+
+  # 82
+  # standalone_hline(lty = 2)
+  # 83
+  # standalone_hline(lty = 2)
+  plot.new()
+  legend(0.5, 0.5,legend = c("Control input"), horiz = TRUE,
+         lty = 1,
+         col = get_color("control"),
+         lwd = 1.5, cex = 0.9, xjust=0.5, yjust=0.5, bty = "n")
+  plot.new()
+  # standalone_hline(lty = 1)
+  # p <- recordPlot()
+
+  # dev.off()
+
+  # return(list(
+  #   plot = p,
+  #   width = sum(widths),
+  #   height = sum(heights)
+  # ))
+}
+
+add_vertex_edge <- function(x, vertex_name, vertex_type, edges_from, edges_to, edges_type){
+  x$edges <- x$edges %>%
+    rbind(dplyr::data_frame(from = edges_from, to = edges_to, control_type = edges_type))
+  x$vertex <- x$vertex %>%
+    rbind(dplyr::data_frame(name = vertex_name, control_type = vertex_type))
+  x
+}
+
+
+#' Title
+#'
+#' @param x an incidence matrix or an igraph network. If an igraph network the higher level argument must be specified
+#' @param type type of directed links desired. Asymmetry produces a single
+#' @param higher_level
+#'
+#' @return
+#' @export
+#'
+#' @examples
+as_directed_network <- function(x,
+                                direction = c("asymmetry", "dependence", "top-down", "bottom-up"),
+                                ties = c("both", "none", "top-down", "bottom-up"), higher_level){
+  # if is an igraph object construct an incidence matrix with the weights
+  if(class(x) == "igraph"){
+    types <- dplyr::data_frame(name = igraph::V(x)$name, type = igraph::V(x)$type)
+    x <- x %>% igraph::as_incidence_matrix(types = igraph::V(.)$type == higher_level, attr = "weight")
+  } else {
+    types <- dplyr::data_frame(name = c(rownames(x), colnames(x)),
+                               type = c(rep(F, nrow(x)), rep(T, ncol(x))))
+  }
+  if(direction[1] == "top-down"){
+    y <- igraph::graph_from_incidence_matrix(x, directed = T, "in", weighted = T)
+  } else if(direction[1] == "bottom-up"){
+    y <- igraph::graph_from_incidence_matrix(x, directed = T, "out", weighted = T)
+  } else if(direction[1] %in% c("asymmetry", "dependence")){
+    y <- x %>%
+      bipartite::linklevel("dependence")
+    if(direction[1] == "asymmetry"){
+      y <- y %>% get_assymetry(ties = ties)
+    }
+    y <- two_incidence_to_adjacency(y)
+    y <- igraph::graph_from_adjacency_matrix(y, mode = "directed", weighted = T)
+  }
+  y %>%
+    assign_vertex_types(types)
+}
+
+assign_vertex_types <- function(x, types){
+  net_names <- dplyr::data_frame(name = igraph::V(x)$name)
+  net_name_type <- net_names %>% dplyr::inner_join(types, by = "name")
+  igraph::set_vertex_attr(x, "type", value = net_name_type$type)
+}
+
+two_incidence_to_adjacency <- function(a){
+  x <- a[[1]]
+  y <- a[[2]]
+  # fill "inverse" diagonal
+  adj_matrix <- rbind(
+    cbind(matrix(0,nrow(x), nrow(x)), x),
+    cbind(t(y), matrix(0, ncol(x), ncol(x))))
+  colnames(adj_matrix) <- rownames(adj_matrix)
+  adj_matrix
+}
+
+get_assymetry <- function(a, ties){
+  if(ties[1]=="both"){
+    r <- c(1,1)
+  } else if(ties[1]=="none"){
+    r <- c(0,0)
+  } else if(ties[1]=="bottom-up"){
+    r <- c(1,0)
+  } else if(ties[1]=="top-down"){
+    r <- c(0,1)
+  }
+  list(
+    (a[[1]]-a[[2]])/pmax(a[[1]], a[[2]]),
+    (a[[2]]-a[[1]])/pmax(a[[1]], a[[2]])
+  ) %>%
+    purrr::map2(r, replace_zeros) %>%
+    purrr::map(replace_negatives) %>%
+    purrr::map(replace_nas)
+
+}
+
+replace_negatives <- function(x, y = 0){
+  x[x<0] <- y
+  x
+}
+
+replace_nas <- function(x, y = 0){
+  x[is.na(x)] <- y
+  x
+}
+
+replace_zeros <- function(x,y = 0){
+  x[x==0] <- y
+  x
+}
+
+# common formatting
+ntw_format_theme <- function(x, named = T){
+  x %<>%
+    fancify_vertex_name() %>%
+    add_property(element = "edge", attr_name = "color", attr_base = "type", 'TRUE ~ "black"') %>%
+    add_property(element = "edge", attr_name = "label.color", attr_base = "type",'TRUE ~ "black"') %>%
+    add_property(element = "edge", attr_name = "label.font", attr_base = "type",'TRUE ~ 2') %>%
+    add_property(element = "vertex", attr_name = "color",attr_base = "type", "TRUE ~ get_color('base')") %>%
+    add_property(element = "vertex", attr_name = "frame.color", attr_base = "type","TRUE ~ 'black'") %>%
+    add_property(element = "vertex", attr_name = "size",attr_base = "type", "TRUE ~ 55") %>%
+    add_property(element = "vertex", attr_name = "label.cex", attr_base = "type","TRUE ~ 1") %>%
+    add_property(element = "vertex", attr_name = "label.color", attr_base = "type","TRUE ~ 'black'") %>%
+    add_property(element = "edge",attr_name = "arrow.size", attr_base = "control_type", "TRUE ~ 0.4") %>%
+    add_property(element = "edge",attr_name = "arrow.width", attr_base = "control_type", "TRUE ~ 0.7")
+
+  if(!named) x %<>% add_property(element = "vertex", attr_name = "name_fancy", attr_base = "type", 'TRUE ~ NA')
+  return(x)
+
+}
+
+#' Make a vertex name a mathematica expression
+#'
+#' @param x an igraph network with vertex names
+#'
+#' @return an igraph network
+#'
+fancify_vertex_name <- function(x){
+
+  n <- igraph::vertex_attr(x, "name") %>%
+    purrr::array_branch() %>%
+    purrr::map_chr(fancify_vertex_ind_name)
+
+  igraph::V(x)$name_fancy <- n %>%
+    latex2exp::TeX()
+  x
+}
+
+fancify_vertex_ind_name <- function(y){
+  if(length(stringr::str_match_all(y, "_")[[1]]) == 1){
+    yy <- stringr::str_split(y, "_") %>%
+      extract2(1) %>% {
+        paste0(.[1], "_{", .[2], "}")
+      }
+  } else {
+    yy <- y
+  }
+  paste0("$", yy, "$")
+}
+
+#' Add a property for vertex or graphs
+#'
+#' @param x an igraph network
+#' @param element either "vertex" or "edge"
+#' @param ... named argument. The name of the argument correspond to the new attribute and
+#'
+#' @return
+#'
+add_property <- function(x, element, attr_name = "type", attr_base, ...){
+  if(element == "vertex"){
+    sel <- igraph::set_vertex_attr
+    elements <- igraph::V(x)
+    get_attr <- igraph::vertex_attr
+  } else {
+    sel <- igraph::set_edge_attr
+    elements <- igraph::E(x)
+    get_attr <- igraph::edge_attr
+  }
+  type <- get_attr(x, attr_base)
+  if(is.null(type)) type <- 1:length(elements)
+  e <- environment()
+  pattern <- list(...) %>% purrr::map(as.formula, e)
+  df <- dplyr::data_frame(type = type) %>%
+    dplyr::mutate(attr = dplyr::case_when(!!! pattern))
+  sel(x, name = attr_name, value = df$attr)
+
+}
+
+get_color <- function(x = T){
+  dplyr::case_when(
+    x == "matched" ~ my_pallete()$dark_purple,
+    x == "unmatched" ~ my_pallete()$light_purple,
+    x == "control" ~ my_pallete()$dark_orange,
+    x == "base" ~ my_pallete()$light,
+    x == "dark" ~ "grey10",
+    x == "bg" ~ "grey95",
+    x == "bg_dark" ~ "grey90",
+    TRUE ~ "white"
+  )
+}
+
+my_pallete <- function(){
+  list(dark_orange = "#e66101",
+       light_orange = "#fdb863",
+       light_purple = "#b2abd2",
+       extra_light_purple = "#d1cde4",
+       dark_purple = "#5e3c99",
+       dark_grey = "#636363",
+       light_grey = "#bdbdbd",
+       light = "#f7f7f7")
+}
+
+
+ntw_control_network_theme <- . %>%
+  add_property(element = "edge", attr_name = "color", attr_base ="control_type", 'type== "a" ~ "black"', 'TRUE ~ get_color("control")') %>%
+  add_property(element = "edge",attr_name = "lty", attr_base = "control_type", "type == 'a' ~ 1", "TRUE ~ 1") %>%
+  add_property(element = "vertex", attr_name = "frame.color", attr_base = "control_type", "type == 'a' ~ 'black'", "TRUE ~ get_color('bg')") %>%
+  add_property(element = "vertex", attr_name = "color", attr_base = "control_type", "type == 'b' ~ get_color('bg')", "TRUE ~ get_color('base')") %>%
+  add_property(element = "vertex", attr_name = "size",attr_base = "control_type",  "type == 'a' ~ 55", "TRUE ~ 45")
+
+standalone_text <- function(text, x = 0.5, y = 0.5, adj = c(0.5,0.5), font = 1, cex = 0.9, srt = 0){
+  plot.new()
+  text(x, y, text, adj = adj, font = font, cex = cex, srt = srt)
+}
+
+
+#' Rescale igraph layout
+#'
+#' Use with rescale=FALSE in igraph::plot
+#'
+#' @param layout the layout -a matrix-
+#' @param xlim a vector of length two with the minimum and maximum x value
+#' @param ylim a vector of length two with the minimum and maximum y value
+#'
+#' @return the updated layout
+#'
+rescale_layout <- function(layout, xlim, ylim){
+  layout[, 1] <- range02(layout[, 1], xlim[1], xlim[2])
+  layout[, 2] <- range02(layout[, 2], ylim[1], ylim[2])
+  layout
+}
+
+
+#' Rescale range of a vector
+#'
+#'
+#' @param x vector to be rescaled
+#' @param newMin new minimum
+#' @param newMax new maximum
+#'
+#' @return the updated layout
+#'
+range02 <- function(x, newMin, newMax){
+  if(min(x) == max(x)) return(x)
+  (x - min(x))/(max(x)-min(x)) * (newMax - newMin) + newMin
+}
+
+direction_layout <- function(){
+  matrix(c(1,0,
+           2,0,
+           3,0,
+           1.5, 1,
+           2.5, 1), nrow = 5, ncol = 2, byrow = T)
+}
+
+
+plot_example_ntw <- function(x, ...){
+  plot_examples(x, rescale = F,
+                frame = F,
+                margin = rep(0, 4),
+                edge.arrow.size = 0.4,
+                edge.label.family = "sans",
+                vertex.label.family = "sans", ...)
+}
+
+#' Plot a network using several aesthetic parameters
+#'
+#' @param x network
+#' @param ... parameters for plot.igraph
+#'
+#'
+plot_examples <- function(x, ...){
+  igraph::plot.igraph(x,
+                      vertex.label = igraph::V(x)$name_fancy,
+                      vertex.label.cex = igraph::V(x)$label.cex,
+                      vertex.label.color = igraph::V(x)$label.color,
+                      vertex.size = igraph::V(x)$size,
+                      vertex.color = igraph::V(x)$color,
+                      vertex.frame.color = igraph::V(x)$frame.color,
+                      vertex.shape = igraph::V(x)$shape,
+                      # vertex.label.family = fam,
+                      # vertex.label.color = l_c,
+                      edge.width = igraph::E(x)$width,
+                      edge.lty = igraph::E(x)$lty,
+                      edge.color = igraph::E(x)$color,
+                      edge.label = igraph::E(x)$label,
+                      edge.label.x = igraph::E(x)$label.x,
+                      edge.label.color = igraph::E(x)$label.color,
+                      edge.label.font = igraph::E(x)$label.font,
+                      edge.arrow.size = igraph::E(x)$arrow.size,
+                      edge.arrow.width = igraph::E(x)$arrow.width,
+                      # edge.label.family = fam,
+                      ...)
+}
