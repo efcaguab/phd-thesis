a4e5ac279059ef1493080c620176c4035a75f936
Fernando Cagua <efcaguab@gmail.com>
2019-03-29 14:12:24 +1300

    Add control capacity figure

---
+++ thesis/code/driver_species/fig4_control-capacity.R
@@ -0,0 +1,57 @@
+make_fig_control_capacity <- function(sl_characteristics, metadata){
+  require(ggplot2)
+
+
+  p5 <- sl_characteristics %>%
+    filter_networks_df(metadata) %>%
+    dplyr::filter(guild == "pla") %>%
+    ggplot(aes(x = control_capacity, colour = guild)) +
+    stat_density(geom = "line",
+                 bw = "SJ",
+                 show.legend = FALSE) +
+    geom_point(aes(x = 1, y = 0),
+               fill = my_pallete()$dark_orange,
+               colour = "black",
+               shape = 21,
+               size = 1) +
+    scale_color_manual(values = my_pallete()$dark_orange) +
+    base_ggplot_theme() +
+    labs(title = "(a) Control capacity of plants",
+         x = latex2exp::TeX("Control capacity ($\\phi$)"),
+         y = "Probability density") +
+    scale_y_continuous(limits = c(0,9)) +
+    theme(legend.position = c(1, 0),
+          legend.justification = c(1,0),
+          legend.background = element_rect(fill = "NA"),
+          legend.key.size = unit(0.15, "in"),
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "mm"))
+
+  p6 <- sl_characteristics %>%
+    filter_networks_df(metadata) %>%
+    dplyr::filter(guild == "pol") %>%
+    ggplot(aes(x = control_capacity, colour = guild)) +
+    stat_density(geom = "line",
+                 bw = "SJ",
+                 show.legend = FALSE) +
+    scale_color_manual(values = my_pallete()$dark_purple) +
+    base_ggplot_theme() +
+    labs(title = "(b) Control capacity of pollinators",
+         x = latex2exp::TeX("Control capacity ($\\phi$)"),
+         y = "Probability density") +
+    scale_y_continuous(limits = c(0,9)) +
+    theme(legend.position = c(1, 0),
+          legend.justification = c(1,0),
+          legend.background = element_rect(fill = "NA"),
+          legend.key.size = unit(0.15, "in"),
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "mm"))
+
+  list(p5, p6)
+}
+
+filter_networks_df <- function(x, metadata, desired_ntw = c("bartomeus", "lopezaraiza")){
+  m <- dplyr::select(metadata, net_name, study)
+  x %>%
+    dplyr::inner_join(m, by = "net_name") %>%
+    dplyr::filter(study %in% desired_ntw) %>%
+    dplyr::select(-study)
+}
