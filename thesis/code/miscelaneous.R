addb35c9a4ff970bf9effdebd649696179ccad3a
Fernando Cagua <efcaguab@gmail.com>
2019-09-16 18:54:19 +1200

    remove url and urldate from references

--- thesis/code/miscelaneous.R
+++ thesis/code/miscelaneous.R
@@ -2,9 +2,15 @@
 get_bibliography <- function(filename_in, filename_out){
 
   biblio_items <- readLines(filename_in)
-  writeLines(biblio_items, filename_out)
 
-  biblio_items %>%
+  url_lines <- stringr::str_detect(biblio_items, "url = ") |
+    stringr::str_detect(biblio_items, "urldate = ")
+
+  writeLines(biblio_items[!url_lines],
+             filename_out)
+
+  biblio_items[!url_lines] %>%
     paste(collapse = " ") %>%
     openssl::sha1()
+
 }
