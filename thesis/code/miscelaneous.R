15152a1f9d4f85b96dd26e242ae8cdd8c9e73a5e
Fernando Cagua <efcaguab@gmail.com>
2019-09-16 18:16:37 +1200

    Add sha to biblio download so we can trigger another thing when it updates

--- thesis/code/miscelaneous.R
+++ thesis/code/miscelaneous.R
@@ -1,5 +1,10 @@
 # download bibliography from a remote file
 get_bibliography <- function(filename_in, filename_out){
-  readLines(filename_in) %>%
-    writeLines(filename_out)
+
+  biblio_items <- readLines(filename_in)
+  writeLines(biblio_items, filename_out)
+
+  biblio_items %>%
+    paste(collapse = " ") %>%
+    openssl::sha1()
 }
