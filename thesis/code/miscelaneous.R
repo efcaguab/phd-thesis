2f2105e13d0f102495c71f0d7d80541c509b23df
Fernando Cagua <efcaguab@gmail.com>
2019-09-27 12:24:53 +1200

    do not remove abstract after all. Problems with multiline abstracts

--- thesis/code/miscelaneous.R
+++ thesis/code/miscelaneous.R
@@ -4,8 +4,7 @@ get_bibliography <- function(filename_in, filename_out){
   biblio_items <- readLines(filename_in)
 
   lines_to_remove <- stringr::str_detect(biblio_items, "url = ") |
-    stringr::str_detect(biblio_items, "urldate = ") |
-    stringr::str_detect(biblio_items, "abstract = ")
+    stringr::str_detect(biblio_items, "urldate = ")
 
   writeLines(biblio_items[!lines_to_remove],
              filename_out)
