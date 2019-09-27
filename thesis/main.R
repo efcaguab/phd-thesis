ecc37ccb1bbc44d976ea103ee67a1f61a6ccd317
Fernando Cagua <efcaguab@gmail.com>
2019-09-27 12:26:50 +1200

    Take all bibliography from the one phd-biblio file

--- thesis/main.R
+++ thesis/main.R
@@ -10,14 +10,6 @@ f <- lapply(list.files("code",
 # Create bibliography folder if it doesn't exist
 dir.create("biblio", showWarnings = FALSE)
 biblio_plan <- drake_plan (
-pollen_competition = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/pollen-competition.bib",
-                                      file_out("biblio/pollen-competition.bib")),
-network_control = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/network-control.bib",
-                                   file_out("biblio/network-control.bib")),
-interactions_sdm = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/interactions%2Bsdm.bib",
-                                    file_out("biblio/interactions_sdm.bib")),
-ownpubs = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/ownpubs.bib",
-                           file_out("biblio/ownpubs.bib")),
 target(command = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/phd-literature.bib",
                                   file_out("biblio/phd-literature.bib"))))
 
