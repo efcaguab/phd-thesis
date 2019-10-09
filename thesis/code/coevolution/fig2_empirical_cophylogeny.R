2056340fd31291e3716ec63c091465e0ace5a5ac
Fernando Cagua <efcaguab@gmail.com>
2019-10-09 17:20:01 +1300

    First go to  main results figure coevolution

---
+++ thesis/code/coevolution/fig2_empirical_cophylogeny.R
@@ -0,0 +1,133 @@
+pre_process_coev_data <- function(barber, random, paco, phylo_signal, aic_obs, aic_same, comm){
+
+  #modules
+  barber <- readRDS(barber)
+  random <- readRDS(random) %>%
+    dplyr::select(network, permutation, modularity)
+
+  mod <- dplyr::bind_rows(barber, random)
+  mod <- plyr::ddply(mod, "network", function(x){
+    x %<>%
+      dplyr::mutate(rank = dplyr::min_rank(modularity)/dplyr::n())
+  })
+
+  mod %<>%
+    dplyr::filter(is.na(permutation)) %>%
+    dplyr::mutate(mod = 1 - rank,
+                  modularity = replace(mod, mod < 1/1000, 1/1000)) %>%
+    dplyr::select(network, modularity)
+
+  # comm cophylogeny
+  paco <- read.csv(paco, row.names=1)
+  paco <- subset(paco, paco$type == 'HP_S')
+  paco <- paco[,c(1,4)]
+  colnames(paco) <- c('network', 'coevolution')
+
+  # phylogenetic signal pollinators
+  phy <- read.csv(phylo_signal, row.names=1)
+  phy <- phy %>%
+    dplyr::group_by(network, type) %>%
+    dplyr::group_by() %>%
+    dplyr::select(network, type, pval) %>%
+    tidyr::spread(type, pval) %>%
+    dplyr::rename(phy_pol = pol) %>%
+    dplyr::rename(phy_pla = pla) %>%
+    dplyr::select(network, phy_pol, phy_pla)
+
+  # module aic
+  aic_obs <- read.csv(aic_obs, sep=',', row.names=1, header=TRUE)
+  aic_obs <- subset(aic_obs, aic_obs$type=='HP_S')
+  aic_obs$perm <- as.character(aic_obs$perm)
+
+  aic_same <- read.csv(aic_same, sep=',', header=TRUE, row.names=1)
+  aic_same <- subset(aic_same, aic_same$type=='HP_S')
+
+  aic <- rbind(aic_same, aic_obs)
+  aic <- aic %>%
+    dplyr::group_by(network, type) %>%
+    dplyr::mutate(aic = dplyr::percent_rank(aic)) %>%
+    dplyr::filter(perm == "rds") %>%
+    dplyr::select(-X1, -perm) %>%
+    dplyr::group_by() %>%
+    dplyr::mutate(type = paste("aic", type, sep = "_")) %>%
+    tidyr::spread(type, aic)
+
+  # shuffled assemblage paco
+  comm <- read.csv(comm, row.names=1)
+  colnames(comm) <- c('network', 'comm_just')
+  comm[,1] <- unlist(strsplit(as.character(comm[,1]), '.csv'))
+
+
+  all_results_org <- dplyr::full_join(comm, paco) %>%
+    #dplyr::full_join(mod) %>%
+    dplyr::full_join(phy) %>%
+    dplyr::full_join(dplyr::select(aic, network, aic_HP_S)) %>%
+    tidyr::gather(property, p_value, -network) %>%
+    #   mutate(p_value = replace(p_value, p_value < 1/1000, 1/1000),
+    #          p_value = replace(p_value, p_value > 999/1000, 999/1000))
+    dplyr::filter(!is.na(p_value)) %>%
+    dplyr::distinct()
+
+  all_results <- all_results_org %>%
+    dplyr::mutate(property = plyr::mapvalues(property,
+                                from = c("comm_just",
+                                         "coevolution",
+                                         #"modularity",
+                                         "phy_pol",
+                                         "phy_pla",
+                                         "aic_HP_S"),
+
+                                to = c("Community CS (shuffled assemblage)",
+                                       "Community CS (shuffled network)",
+                                       #"Community modularity",
+                                       "Phylogenetic signal (pollinator modules)",
+                                       "Phylogenetic signal (plant modules)",
+                                       "Modular network CS")))
+
+
+  prop_matrix <- all_results %>%
+    dplyr::mutate(signif = p_value <= 0.05,
+           signif = plyr::mapvalues(signif,
+                                    c(TRUE, FALSE),
+                                    c(1, 0))) %>%
+    dplyr::select(network, property, signif) %>%
+    reshape2::acast(network ~ property, value.var = "signif")
+
+  commi <- prop_matrix[,3]
+  prop_matrix <- prop_matrix[,-3]
+  prop_matrix <- cbind(prop_matrix, commi)
+  colnames(prop_matrix)[5] <- "Modular network CS"
+
+  comco <- prop_matrix[,2]
+  prop_matrix <- prop_matrix[,-2]
+  prop_matrix <- cbind(comco, prop_matrix)
+  colnames(prop_matrix)[1] <- "Community CS (shuffled network)"
+
+
+  prop_matrix[is.na(prop_matrix)] <- 0
+
+  all_results %>%
+    dplyr::mutate(network = factor(network, levels = rownames(prop_matrix)),
+           property = factor(property, levels = rev(colnames(prop_matrix))))
+
+}
+
+make_fig_empirical_cophylogeny <- function(data_coevolution_res){
+  data_coevolution_res %>%
+    dplyr::mutate(
+      network = forcats::fct_reorder(network, p_value, .fun = min),
+      group = cut(as.numeric(network), 3)) %>%
+    ggplot() +
+    geom_tile(aes(x = network, y = property, fill = plogis(p_value), width = 0.8, height = 0.8),
+              alpha = 0.8) +
+    # scale_fill_brewer(palette = "Paired", name = "", labels = c("p < 0.05", "p > 0.05")) +
+    # coord_fixed() +
+    facet_wrap(~ group, scales = "free_x", ncol = 1) +
+    # scale_x_discrete(labels = c(1:54))+
+    xlab('Pollination networks') +
+    base_ggplot_theme() +
+    theme(legend.position = "top",
+          axis.title.y = element_blank())
+
+  ggsave("plot1.pdf", width = fig_sizes()$two_column_width, height = fig_sizes()$two_column_width)
+}
--- thesis/main.R
+++ thesis/main.R
@@ -13,9 +13,18 @@ biblio_plan <- drake_plan (
 target(command = get_bibliography("https://raw.githubusercontent.com/efcaguab/phd-bibliography/master/phd-literature.bib",
                                   file_out("bibliography/phd-literature.bib"))))
 
-pdf_plan <-
-  drake_plan(thesis = bookdown::render_book(knitr_in("index.Rmd"),
-                                            config_file = file_in("_bookdown.yml")))
+pdf_plan <- drake_plan(
+    data_coevolution_res = pre_process_coev_data(
+      barber = file_in("data/coevolution/barber_modularity_values.rds"),
+      random = file_in("data/coevolution/significancy_modularity_V2.rds"),
+      paco = file_in("data/coevolution/4_paco_shuf_net_res.csv"),
+      phylo_signal = file_in("data/coevolution/phylogenetic_signal.csv"),
+      aic_obs = file_in("data/coevolution/aic_obs.csv"),
+      aic_same = file_in("data/coevolution/aic_same_size.csv"),
+      comm = file_in("data/coevolution/5_paco_shuf_ass_res.csv")
+    ),
+    thesis = bookdown::render_book(knitr_in("index.Rmd"),
+                                   config_file = file_in("_bookdown.yml")))
 
 
 full_plan <- rbind(biblio_plan,
